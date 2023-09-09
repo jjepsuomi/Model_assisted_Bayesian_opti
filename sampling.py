5231a6f767e12198dbabbd2e212957664bfe615b
Jonne Pohjankukka <jjepoh@utu.fi>
2023-09-09 13:18:02 +0300

    Made function more complicated

--- sampling.py
+++ sampling.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
-
+from scipy.stats import chi2
 
 def gaussian(mean=0, std=1, input_x=0):
     gaussian_value = np.exp(-(input_x - mean)**2 / (2 * std**2))
@@ -11,7 +11,22 @@ def gaussian(mean=0, std=1, input_x=0):
 
 def true_function(input_x=0):
     #true_function_values = input_x * np.sin(input_x) + gaussian(5.7, 1, 5, input_x) - gaussian(-8, 2, 40, input_x) - gaussian(8, 0.8, 3, input_x) - gaussian(-4, 1.4, 10, input_x)
-    return input_x * np.sin(input_x) + gaussian(5.7, 1, input_x) - 2*gaussian(-8, 2, input_x) - gaussian(8, 0.8, input_x) - 5*gaussian(-7.5, 1, input_x)
+    true_function_values = 0.1*input_x + input_x * np.sin(input_x) \
+    + gaussian(5.7, 1, input_x) \
+    - 2*gaussian(-8, 2, input_x) \
+    - gaussian(8, 0.8, input_x) \
+    - 5*gaussian(-7.5, 1, input_x) \
+    - 3.7*gaussian(-15,2,input_x) \
+    - 2*gaussian(-11,2,input_x) \
+    - 10*gaussian(-14,1,input_x) \
+    + 5*gaussian(-17.5, 3, input_x) \
+    - 15*gaussian(-20,5,input_x) \
+    + 15*gaussian(-15,2,input_x) \
+    + 2*gaussian(-18,1,input_x) \
+    - 0.5*gaussian(-15,1,input_x) \
+    + np.cos(input_x*2)
+
+    return true_function_values
 
 
 def get_random_sample_from_true_function(xmin=-10, xmax=10, num_samples=1):
@@ -39,8 +54,8 @@ def predictive_uncertainty(input_x, y_mean, y_std):
     return input_x[max_std_ind], y_mean[max_std_ind]
 
 # Generate the data set
-input_x_interval_min = -10
-input_x_interval_max = 10
+input_x_interval_min = -20
+input_x_interval_max = 20
 number_y_points_from_true_function = 100
 print(f'Generating true function {number_y_points_from_true_function} points from x-interval: [{input_x_interval_min}, {input_x_interval_max}]')
 x = np.linspace(input_x_interval_min, input_x_interval_max, number_y_points_from_true_function)
@@ -49,13 +64,18 @@ print(f'Shape of x is: {x.shape}')
 print(f'Shape of y is: {y.shape}')
 
 # Make sample data
-sample_x, sample_y = get_random_sample_from_true_function(-10, 10, 12)
+sample_x, sample_y = get_random_sample_from_true_function(-20, 20, 30)
 sample_x = sample_x.reshape(-1, 1)
 gpr_model = fit_gaussian_process(sample_x.reshape(-1,1), sample_y)
 # Next, predict the function
 predicted_y_mean, predicted_y_std = gpr_model.predict(X=x.reshape(-1,1), return_std=True)
 # Get the predictive uncertainty point
-xopt, yopt = predictive_uncertainty(x, y, predicted_y_std)
+xopt, yopt = predictive_uncertainty(x, predicted_y_mean, predicted_y_std)
+
+###########
+## Plotting
+###########
+
 # Create the plot of true function
 plt.figure(figsize=(8, 6))  # Adjust the figure size if needed
 plt.plot(x, y, label='target function', color='blue', linestyle='--')  # Plot the sine function
@@ -65,20 +85,12 @@ plt.fill_between(x, predicted_y_mean - 1.96*predicted_y_std, predicted_y_mean + 
 plt.xlabel('Explanatory variable value')
 plt.ylabel('Function value')
 
-# Plot each sample as a circle
-for xi, yi in zip(sample_x, sample_y):
-    circle = plt.Circle((xi, yi), 0.2, color='red', alpha=0.5)
-    plt.gca().add_patch(circle)
-
-
-circle = plt.Circle((xopt, yopt), 0.2, color='violet', alpha=0.8)
-plt.gca().add_patch(circle)
-
+plt.scatter(sample_x, sample_y, color="red", label="sample point")
 
 # Set the aspect ratio to be equal
 plt.axis('equal')
 # Set the y-range (limits)
-plt.ylim(-10, 10)  # Replace these values with your desired y-range
+plt.ylim(-22, 20)  # Replace these values with your desired y-range
 #plt.xlim(-10, 10)  # Replace these values with your desired y-range
 plt.grid(True)
 plt.legend()
